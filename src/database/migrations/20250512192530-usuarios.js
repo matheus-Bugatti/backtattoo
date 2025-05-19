4102e33d8fa6bff38cdcb6b7ea9fec91f047b0d3
matheus-Bugatti <campos.n@aluno.ifsp.edu.br>
2025-05-19 16:45:21 -0300

    arrumei axo

---
+++ src/database/migrations/20250512192530-usuarios.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        return await queryInterface.createTable('usuarios', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            nome: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
+            },
+            senha: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            token: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false
+            }
+        })
+    },
+
+    async down(queryInterface) {
+        return await queryInterface.dropTable('usuarios')
+    }
+};
